9514618581a735da79f7b753518b231c07a4b593
unknown <nolan.beihl@gmail.com>
2021-12-08 10:32:37 -0700

    Fixed issues with connection to postman, error was in the User model

--- eCommerceStarterCode/Models/User.cs
+++ eCommerceStarterCode/Models/User.cs
@@ -5,11 +5,9 @@ using Microsoft.AspNetCore.Identity;
 namespace eCommerceStarterCode.Models
 {
     public class User : IdentityUser
-    {   [Key]
-
-        public int UserId { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+    {   
+        public string FirstName { get; set; }
+        public string LastName { get; set; }    
 
 
     }
